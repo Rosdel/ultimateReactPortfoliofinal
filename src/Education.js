81870c2f18d759f9c3ab915b11163235eed0242f
Rosdell Ouma <abonyorosdell@gmail.com>
2022-07-21 17:09:19 +0300

    work section

--- src/Education.js
+++ src/Education.js
@@ -1,4 +1,44 @@
 import React from "react"
+import "./education.css"
+
 export default function Education() {
-  return <>Education</>
+  return (
+    <>
+      <div className="d-flex flex-column">
+        <div className="education-one my-4 justify-content-between d-flex flex-row">
+          <div className="d-flex flex-column new-one">
+            <span className="university-name">
+              FACULTY OF TECHNOLOGY BUSITEMA UNIVERSITY
+            </span>
+            <span className="university-degree">
+              BACHELOR OF SCIENCE IN ENGINEERING
+            </span>
+          </div>
+          <div>
+            <span className="yearPassedOut">2016-2021</span>
+          </div>
+        </div>
+        <div className="education-one my-4 justify-content-between d-flex flex-row">
+          <div className="d-flex flex-column new-one">
+            <span className="university-name">
+              MENGO SENIOR SECONDARY SCHOOL
+            </span>
+            <span className="university-degree">ADVANCED CERTIFICATE</span>
+          </div>
+          <div>
+            <span className="yearPassedOut">2013-2014</span>
+          </div>
+        </div>
+        <div className="education-one my-4 justify-content-between d-flex flex-row">
+          <div className="d-flex flex-column new-one">
+            <span className="university-name">LAYIBI COLLEGE</span>
+            <span className="university-degree">U.C.E</span>
+          </div>
+          <div>
+            <span className="yearPassedOut">2009-2012</span>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
--- src/Work.js
+++ src/Work.js
@@ -1,4 +1,30 @@
 import React from "react"
+import "./work.css"
 export default function Work() {
-  return <>Work</>
+  return (
+    <>
+      <div className="work-inside-new my-4 mx-2">
+        <div className="d-flex flex-column">
+          <div className="d-flex flex-row justify-content-between">
+            <span className="compony-name">Nerdy Turtlez</span>
+            <span className="yearPassedOut">2020-2021</span>
+          </div>
+        </div>
+        <div className="d-flex flex-column">
+          <span className="position">Full stack Developer</span>
+          <span className="description-position">
+            <ul>
+              <li>
+                Built websites for client as a freelancer with NerdyTutlez
+              </li>
+              <li>
+                Built an E-commerce websites for client as a freelancer with
+                NerdyTutlez
+              </li>
+            </ul>
+          </span>
+        </div>
+      </div>
+    </>
+  )
 }
