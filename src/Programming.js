e4ddb28178428841a933c66d1807f1e9b699517f
Rosdell Ouma <abonyorosdell@gmail.com>
2022-07-21 18:56:25 +0300

    project

--- src/Programming.js
+++ src/Programming.js
@@ -1,4 +1,45 @@
 import React from "react"
+import "./programming.css"
 export default function Programming() {
-  return <>Programming</>
+  const value = [
+    {
+      text: "Javascript",
+      percentage: 80,
+    },
+    {
+      text: "React.js",
+      percentage: 70,
+    },
+    {
+      text: "Node.js",
+      percentage: 70,
+    },
+    {
+      text: "Python",
+      percentage: 60,
+    },
+  ]
+  return (
+    <>
+      <div>
+        <div className="row">
+          {value.map((value, index) => {
+            return (
+              <>
+                <div className="col-lg-6 col-md-6 col-sm-6 my-2">
+                  <span className="language">{value.text}</span>
+                  <div className="progress-some">
+                    <div
+                      className="progress-new"
+                      style={{ width: `${value.percentage}%` }}
+                    ></div>
+                  </div>
+                </div>
+              </>
+            )
+          })}
+        </div>
+      </div>
+    </>
+  )
 }
--- src/Project.js
+++ src/Project.js
@@ -1,4 +1,41 @@
 import React from "react"
 export default function Project() {
-  return <>Projects</>
+  return (
+    <>
+      <div className="d-flex flex-column">
+        <div className="education-one my-4 justify-content-between d-flex flex-row">
+          <div className="d-flex flex-column new-one">
+            <span className="university-name">Javascript</span>
+            <span className="university-degree">Developed A dice Game</span>
+          </div>
+          <div>
+            <span className="yearPassedOut"></span>
+          </div>
+        </div>
+        <div className="education-one my-4 justify-content-between d-flex flex-row">
+          <div className="d-flex flex-column new-one">
+            <span className="university-name">React.Js </span>
+            <span className="university-degree">
+              Developed an Ecommerce website &#38; this Portfolio
+            </span>
+          </div>
+          <div>
+            <span className="yearPassedOut"></span>
+          </div>
+        </div>
+        <div className="education-one my-4 justify-content-between d-flex flex-row">
+          <div className="d-flex flex-column new-one">
+            <span className="university-name">Node.Js for the Backend </span>
+
+            <span className="university-degree">
+              Implemented payment API for the website
+            </span>
+          </div>
+          <div>
+            <span className="yearPassedOut">2009-2012</span>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
